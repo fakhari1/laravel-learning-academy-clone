3a2503bccf945148d25c700fc6d3f22ed6059ea5
FAKHARI1 <hosein.fakhari1998@gmail.com>
2023-05-27 16:35:53 +0330

    admin template is ready

--- resources/views/admin/partials/_sidebar.blade.php
+++ resources/views/admin/partials/_sidebar.blade.php
@@ -1,68 +1,77 @@
 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
     <div class="position-sticky pt-3">
         <ul class="nav flex-column">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
+            <li class="nav-item d-flex justify-content-between align-items-center ps-2">
+                <div class="d-flex justify-content-between align-items-center">
                     <i class="far fa-dashboard"></i>
-                    داشبورد
-                </a>
+
+                    <a class="nav-link active" aria-current="page" href="{{ route('admin.index') }}">
+                        داشبورد
+                    </a>
+                </div>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
+            <li class="nav-item d-flex justify-content-between align-items-center ps-2">
+                <div class="d-flex justify-content-between align-items-center">
                     <i class="far fa-file"></i>
-                    سفارشات
-                </a>
+
+                    <a class="nav-link" href="{{ route('admin.articles.index') }}">
+                        مقاله ها
+                    </a>
+                </div>
+                <span class="badge bg-info rounded-circle text-black badge-counter">
+                        999
+                    </span>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
+            <li class="nav-item d-flex justify-content-between align-items-center ps-2">
+                <div class="d-flex justify-content-between align-items-center">
                     <i class="far fa-shopping-cart"></i>
-                    محصولات
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <i class="far fa-users"></i>
-                    مشتریان
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <i class="far fa-chart-bar"></i>
-                    گزارشات
-                </a>
+
+                    <a class="nav-link" href="{{ route('admin.courses.index') }}">
+                        دوره های آموزشی
+                    </a>
+                </div>
+                <span class="badge bg-info rounded-circle text-black badge-counter">
+                        999
+                    </span>
             </li>
         </ul>
 
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-            <span>Saved reports</span>
-            <a class="link-secondary" href="#" aria-label="Add a new report">
-                <i class="far fa-plus-circle"></i>
-            </a>
-        </h6>
         <ul class="nav flex-column mb-2">
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <span data-feather="file-text"></span>
-                    Current month
-                </a>
+            <li class="nav-item d-flex justify-content-between align-items-center ps-2">
+                <div class="d-flex justify-content-between align-items-center">
+                    <i class="far fa-users"></i>
+
+                    <a class="nav-link" href="#">
+                        مشتریان
+                    </a>
+                </div>
+                <span class="badge bg-info rounded-circle text-black badge-counter">
+                        999
+                    </span>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <span data-feather="file-text"></span>
-                    Last quarter
-                </a>
+            <li class="nav-item d-flex justify-content-between align-items-center ps-2">
+                <div class="d-flex justify-content-between align-items-center">
+                    <i class="far fa-laugh"></i>
+
+                    <a class="nav-link" href="#">
+                        پرداخت های موفق
+                    </a>
+                </div>
+                <span class="badge bg-info rounded-circle text-black badge-counter">
+                        20
+                    </span>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <span data-feather="file-text"></span>
-                    Social engagement
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <span data-feather="file-text"></span>
-                    Year-end sale
-                </a>
+            <li class="nav-item d-flex justify-content-between align-items-center ps-2">
+                <div class="d-flex justify-content-between align-items-center">
+                    <i class="far fa-sad-cry"></i>
+
+                    <a class="nav-link" href="#">
+                        پرداخت های ناموفق
+                    </a>
+                </div>
+                <span class="badge bg-info rounded-circle text-black badge-counter">
+                        0
+                    </span>
             </li>
         </ul>
     </div>
